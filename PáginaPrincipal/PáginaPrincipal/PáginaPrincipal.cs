33ce79a4186278ff21c29661d9843032b4b34ffc
2170397@my.Ipleiria.pt <33024157+Joelruna@users.noreply.github.com>
2018-05-21 13:41:18 +0100

    GerirCasas Feito

--- PáginaPrincipal/PáginaPrincipal/PáginaPrincipal.cs
+++ PáginaPrincipal/PáginaPrincipal/PáginaPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoC;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,7 +25,8 @@ namespace PáginaPrincipal
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-
+            FormCliente interligar = new FormCliente();
+            interligar.ShowDialog();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
